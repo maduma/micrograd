fbea1a0f873a1114b75db75ca07d1699e5a3fe23
Stéphane Nsakala <stephane.nsakala@hopitauxschuman.lu>
2024-12-27 14:46:00 +0100

    my very simple binary classifier

---
+++ start-jupyterlab.ps1
@@ -0,0 +1,8 @@
+uvx --python 3.12 `
+    --with torch `
+    --with numpy `
+    --with matplotlib `
+    --with graphviz `
+    --with micrograd `
+    --with scikit-learn `
+    --with jupyterlab --from jupyter-core jupyter lab
